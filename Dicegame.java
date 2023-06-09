94bf7d822a54eb712fd43cace2e517408894632e
unreachableluv <109418921+unreachableluv@users.noreply.github.com>
2023-06-09 13:27:12 +0900

    Dice game: initial revision

---
+++ Dicegame.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class Dicegame{
+    public static void main(String[] args){
+	Random rand = new Random();  
+
+    int num1 = rand.nextInt(6) + 1;
+    int num2 = rand.nextInt(6) + 1;
+
+    int total = num1 + num2;
+
+    System.out.println("Die 1:"+num1);
+    System.out.println("Die 2:"+num2);
+    System.out.println("Total value:" +total);
+	
+    }
+}
